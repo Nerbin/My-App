33dbcf2bf1526ab1a641b2a34e9b72cc9797423a
Ivan <mokrakivan17@gmail.com>
2024-09-26 22:16:23 +0300

    solutions to the second task

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -3,6 +3,6 @@ package app;
 public class MainApp {
     public static void main(String[] args) {
 
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
